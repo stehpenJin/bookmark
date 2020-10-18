34180964fcee0482ec69c800403485f9eff5244d
stehpenjin <sidnyhall@googlemail.com>
2020-10-18 18:15:12 +0900

    change mainpg

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import bookmark.views
 
 urlpatterns = [
+    path('',bookmark.views.BookmarkListView.as_view()),
     path('admin/', admin.site.urls),
     path('bookmark/', include('bookmark.urls'))
 ]
